e1182706c665f0105cd5cb85d925abfc6e30d411
Collin Gentle <collingentle@gmail.com>
2025-04-09 21:07:47 -0500

    align and modify blog cards in individual pages

--- shaw-journal/src/app/entertainment/page.tsx
+++ shaw-journal/src/app/entertainment/page.tsx
@@ -33,10 +33,13 @@ export default function EntertainmentPage() {
 	{loading && <p>Loading...</p>}
         {error && <p className="text-red-500">{error}</p>}
 
+        <div className="sticky top-0 z-10 bg-primary py-2">
+        <h1 className="text-3xl font-bold text-center text-black">Entertainment Blog</h1>
+        </div>
 
         {/* Display Blogs */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-		{renderBlogCards(entertainmentBlogs)}
+        <div className="flex flex-col space-y-10 max-w-3xl mx-auto p-6 bg-primary">
+        {renderBlogCards(entertainmentBlogs)}
         </div>
 
       </Container>
--- shaw-journal/src/app/tech/page.tsx
+++ shaw-journal/src/app/tech/page.tsx
@@ -32,9 +32,13 @@ export default function TechPage() {
 	{loading && <p>Loading...</p>}
         {error && <p className="text-red-500">{error}</p>}
 
+        <div className="sticky top-0 z-10 bg-primary py-2">
+        <h1 className="text-3xl font-bold text-center text-black">Technology Blog</h1>
+        </div>
+
         {/* Display Blogs */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">	
-		{renderBlogCards(technologyBlogs)}
+        <div className="flex flex-col space-y-10 max-w-3xl mx-auto p-6 bg-primary">
+        {renderBlogCards(technologyBlogs)}
         </div>
 
       </Container>
